252e8aa16768bc79f7d417f428ffbf9d9fd5e3c6
nemaego <nemaego@yandex.ru>
2024-10-27 18:43:41 +0700

    feat: add func

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -124,7 +124,7 @@ function removeFalsyValues(arr) {
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
 function getUpperCaseStrings(arr) {
-  return arr.map(item => )
+  return arr.map((item) => item.toUpperCase());
 }
 
 
